1865e655e9d9d8da52a26e60b940cdbc14bc47ac
Mirza Zenkic <moerzi@gmail.com>
2023-12-24 09:13:17 +0100

    Add start Method to TicTacToe.

--- src/main/java/at/fhtw/TicTacToe.java
+++ src/main/java/at/fhtw/TicTacToe.java
@@ -6,6 +6,25 @@ public class TicTacToe {
     private Player player2;
     private Player currentPlayer;
     private Board board;
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            playGame(scanner);
+
+            System.out.print("Do you want to play again? (Y/N): ");
+            char playAgain = scanner.next().charAt(0);
+
+            if (playAgain == 'Y' || playAgain == 'y') {
+                board.clear();
+                currentPlayer = player1;
+            } else {
+                break;
+            }
+        } while (true);
+
+        scanner.close();
+    }
     private void playGame(Scanner scanner) {
         boolean gameWon = false;
 
@@ -22,6 +41,10 @@ public class TicTacToe {
                 System.out.println("Invalid move. Please try again.");
             }
         } while (!board.isFull() && !gameWon);
+    }
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.start();
     }
 }
 
